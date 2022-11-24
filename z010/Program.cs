b85c1c1d9484bb4e594e1892361eb50dfd126515
andrey <ai.poteryaev@gmail.com>
2022-11-24 14:28:09 +0500

    dz2 zadacha 10

---
+++ z010/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трезначное число");
+
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if (!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+            
+        else
+            break;
+    }
